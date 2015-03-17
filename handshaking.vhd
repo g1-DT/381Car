5385a8502f56c5d5db8aaa25d114082a65bc5784
g1-DT <dhttubc@gmail.com>
2015-03-16 19:42:47 -0700

    Added output, but cannot split GPIO_0 for both input and output

--- handshaking.vhd
+++ handshaking.vhd
@@ -1,27 +1,20 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.NUMERIC_STD.ALL; --necessary?
 
 ENTITY handshaking IS
 PORT(
 	CLOCK_50 : in std_logic;
 	KEY : in std_logic_vector(3 downto 0); --for testing
 	SW : in std_logic_vector(17 downto 0); --for testing/have an on/off switch here?
-	GPIO_0 : in std_logic_vector(35 downto 0); --to communicate with Pi via Breadboard
-	--GPIO_1 : in std_logic_vector(35 downto 0);
+	GPIO_0 : in std_logic_vector(35 downto 0); --to retrieve data from Pi via Breadboard
+	GPIO_1 : out std_logic_vector(35 downto 0) --to send data to Pi via Breadboard
 );
 END handshaking;
-
---necessary?:
---ARCHITECTURE structural OF handshaking IS
---BEGIN
---END structural;
 
 ARCHITECTURE behavioural OF handshaking IS
 	SIGNAL D0_PRIME, D0_IN, D1_PRIME, D1_IN : std_logic_vector(7 downto 0);
 	SIGNAL VALID_PRIME, VALID_IN, READY, ACKNOWLEDGE : std_logic;
 BEGIN
-	--retrieve data from GPIO
 	PROCESS(CLOCK_50)
 	BEGIN
 		IF(RISING_EDGE(CLOCK_50)) THEN
@@ -33,10 +26,21 @@ BEGIN
 	
 	PROCESS(CLOCK_50)
 	BEGIN
-		IF(RISING_EDGE(CLOCK)) THEN
+		IF(RISING_EDGE(CLOCK_50)) THEN
 			D0_IN <= D0_PRIME;
 			D1_IN <= D1_PRIME;
 			VALID_IN <= VALID_PRIME;
+		END IF;
+	END PROCESS;
+	
+	--output
+	PROCESS(CLOCK_50)
+	BEGIN
+		IF(RISING_EDGE(CLOCK_50)) THEN
+			IF(VALID_IN = '1') THEN
+				GPIO_1(7 downto 0) <= D0_IN;
+				GPIO_1(15 downto 8) <= D1_IN;
+			END IF;
 		END IF;
 	END PROCESS;
 END behavioural;
